385d18b34218aae819bbc945a007a9aea605aa86
Andy <andrew@mcgee.technology>
2021-01-28 18:05:44 +1100

    Added Ampache credentials to conf file.

--- config/foam.conf.php
+++ config/foam.conf.php
@@ -1,10 +1,15 @@
 <? php
 // Config file for foam web player. Just a standard php include that will pull in variables
 
+//Ampache Server - location and credentials
+ampurl = 'https://localhost:8080';
+ampusr = 'username';
+amppas = 'passwd';
+
 //Themes - set some basic colours from Fomantic UI
 
-txtcolr1 = black;
-elmcolr1 = blue;
-elmcolr2 = green;
+txtcolr1 = 'black';
+elmcolr1 = 'blue';
+elmcolr2 = 'green';
 
 ?>

58dd3a7d2f9da8c3a9df5ce8fd6059016639dce5
Andy <andrew@mcgee.technology>
2021-01-28 18:13:41 +1100

    Fix typos in conf file variable names.

--- config/foam.conf.php
+++ config/foam.conf.php
@@ -2,14 +2,14 @@
 // Config file for foam web player. Just a standard php include that will pull in variables
 
 //Ampache Server - location and credentials
-ampurl = 'https://localhost:8080';
-ampusr = 'username';
-amppas = 'passwd';
+$ampurl = 'https://localhost:8080';
+$ampusr = 'username';
+$amppas = 'passwd';
 
 //Themes - set some basic colours from Fomantic UI
 
-txtcolr1 = 'black';
-elmcolr1 = 'blue';
-elmcolr2 = 'green';
+$txtcolr1 = 'black';
+$elmcolr1 = 'blue';
+$elmcolr2 = 'green';
 
 ?>

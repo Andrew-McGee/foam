2dbdafaea5bcbc891592784e6c4c416ffc7a4b1d
Andy <andrew@mcgee.technology>
2021-02-03 23:37:06 +1100

    Bold titles in list

--- album_view.php
+++ album_view.php
@@ -72,7 +72,7 @@
 								for ($i = 0; $i < $cnt; $i++){
 									echo '<tr>';
 									echo '<td>' . $song_results[song][$i][track] . '</td>';
-									echo '<td>' . $song_results[song][$i][title] . '</td>';
+									echo '<td><strong>' . $song_results[song][$i][title] . '</strong></td>';
 									echo '<td>' . $song_results[song][$i][artist][name] . '</td>';
 									$result = sec2mins($song_results[song][$i][time]);
 									echo '<td>' . $result[minutes] . ':' . sprintf("%02d", $result[seconds]) . '</td>';

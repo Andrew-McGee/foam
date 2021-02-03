fa48670fc8a6c087bdc21e8811cdf14c662260d6
Andy <andrew@mcgee.technology>
2021-02-03 17:06:09 +1100

    Fixed icons in menu include.

--- includes/menu.php
+++ includes/menu.php
@@ -12,44 +12,44 @@ function active_menu($page){
     echo '<a class="item" href="index.php"><i class="clock icon"></i>Recent</a>';
   }
   if ($page == 'newest') {
-    echo '<a class="active item" href="newest_view.php"><i class="clock icon"></i>Newest</a>';
+    echo '<a class="active item" href="newest_view.php"><i class="meteor icon"></i>Newest</a>';
   } else {
-    echo '<a class="item" href="newest_view.php"><i class="clock icon"></i>Newest</a>';
+    echo '<a class="item" href="newest_view.php"><i class="meteor icon"></i>Newest</a>';
   }
   if ($page == 'artists') {
-    echo '<a class="active item" href="artists_view.php"><i class="clock icon"></i>Artists</a>';
+    echo '<a class="active item" href="artists_view.php"><i class="user icon"></i>Artists</a>';
   } else {
-    echo '<a class="item" href="artists_view.php"><i class="clock icon"></i>Artists</a>';
+    echo '<a class="item" href="artists_view.php"><i class="user icon"></i>Artists</a>';
   }
   if ($page == 'albums') {
-    echo '<a class="active item" href="albums_view.php"><i class="clock icon"></i>Albums</a>';
+    echo '<a class="active item" href="albums_view.php"><i class="record vinyl icon"></i>Albums</a>';
   } else {
-    echo '<a class="item" href="albums_view.php"><i class="clock icon"></i>Albums</a>';
+    echo '<a class="item" href="albums_view.php"><i class="record vinyl icon"></i>Albums</a>';
   }
   if ($page == 'tracks') {
-    echo '<a class="active item" href="tracks_view.php"><i class="clock icon"></i>Tracks</a>';
+    echo '<a class="active item" href="tracks_view.php"><i class="music icon"></i>Tracks</a>';
   } else {
-    echo '<a class="item" href="tracks_view.php"><i class="clock icon"></i>Tracks</a>';
+    echo '<a class="item" href="tracks_view.php"><i class="music icon"></i>Tracks</a>';
   }
   if ($page == 'playlists') {
-    echo '<a class="active item" href="playlists_view.php"><i class="clock icon"></i>Playlists</a>';
+    echo '<a class="active item" href="playlists_view.php"><i class="stream icon"></i>Playlists</a>';
   } else {
-    echo '<a class="item" href="playlists_view.php"><i class="clock icon"></i>Playlists</a>';
+    echo '<a class="item" href="playlists_view.php"><i class="stream icon"></i>Playlists</a>';
   }
   if ($page == 'frequent') {
-    echo '<a class="active item" href="frequent_view.php"><i class="clock icon"></i>Frequent</a>';
+    echo '<a class="active item" href="frequent_view.php"><i class="chart icon"></i>Frequent</a>';
   } else {
-    echo '<a class="item" href="frequent_view.php"><i class="clock icon"></i>Frequent</a>';
+    echo '<a class="item" href="frequent_view.php"><i class="chart icon"></i>Frequent</a>';
   }
   if ($page == 'favourites') {
-    echo '<a class="active item" href="favourites_view.php"><i class="clock icon"></i>Favourites</a>';
+    echo '<a class="active item" href="favourites_view.php"><i class="star icon"></i>Favourites</a>';
   } else {
-    echo '<a class="item" href="favourites_view.php"><i class="clock icon"></i>Favourites</a>';
+    echo '<a class="item" href="favourites_view.php"><i class="star icon"></i>Favourites</a>';
   }
   if ($page == 'random') {
-    echo '<a class="active item" href="random_view.php"><i class="clock icon"></i>Random</a>';
+    echo '<a class="active item" href="random_view.php"><i class="dice icon"></i>Random</a>';
   } else {
-    echo '<a class="item" href="random_view.php"><i class="clock icon"></i>Random</a>';
+    echo '<a class="item" href="random_view.php"><i class="dice icon"></i>Random</a>';
   }
 
   // Build the stats section below menu

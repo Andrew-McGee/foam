37fe9c35078bd24cf73b07c8b1aca5355b16d66c
Andy <andrew@mcgee.technology>
2021-02-03 14:40:58 +1100

    rewrote the API calls include to put repeated CURL code into functions.

--- includes/callAPI.php
+++ includes/callAPI.php
@@ -1,20 +1,52 @@
 <?php
-// callAPI function for all pages that need to call the Ampache API
 //
-
+// Handshake API function for inital authentication to Ampache API
+//
 function handshakeAPI(){
+  $call = 'hshake';
+  $auth = 'new';
+
+  $result = c_init($auth, $call);
+
+  return $result
+}
+
+//
+// Stats API call for recent, newest, frequent, random etc
+//
+function statsAPI($auth, $filter){
+
+  $call = '?action=stats&type=album&limit=24&filter=' . $filter;
+
+  $result = c_init($auth, $call);
+
+  return $result
+}
+
+//
+// Generic CURL stuff used by the various calls to call the Ampache API.
+//
+function c_init($auth, $call){
   include 'config/foam.conf.php';
 
   $curl = curl_init();
 
-  //Build the handshake string
-  $time = time();
-  $key = hash('sha256', $amppas);
-  $passphrase = hash('sha256',$time . $key);
+  //If the call is initial handsake we need to build a different format
+  if ($call == 'hshake') {
 
-  $url = $ampurl.'/server/json.server.php?action=handshake&auth='.$passphrase.'&timestamp='.$time.'&version=350001&user='.$ampusr;
+    //Build the handshake string to get auth key
+    $time = time();
+    $key = hash('sha256', $amppas);
+    $passphrase = hash('sha256',$time . $key);
+    $url = $ampurl.'/server/json.server.php?action=handshake&auth='.$passphrase.'&timestamp='.$time.'&version=350001&user='.$ampusr;
 
-  //Options
+  } else {
+
+    //Non-handshake calls can be built from this format using the url and parms
+    $url = $ampurl . '/server/json.server.php' . $call . '&auth=' . $auth;
+  }
+
+  // CURL options.
   curl_setopt($curl, CURLOPT_URL, $url);
   curl_setopt($curl, CURLOPT_HTTPHEADER, array(
     'Content-Type: application/json',
@@ -27,38 +59,7 @@ function handshakeAPI(){
   $result = curl_exec($curl);
   if(!$result){die("Connection Failure");}
   curl_close($curl);
+
   return $result;
 }
-
-function recentAPI($auth){
-  include 'config/foam.conf.php';
-
-  $curl = curl_init();
-
-  //Build the albums query string
-  $time = time();
-  $key = hash('sha256', $amppas);
-  $passphrase = hash('sha256',$time . $key);
-
-  $url = $ampurl.'/server/json.server.php?action=albums&auth='.$auth.'&limit=24';
-
-  //Options
-  curl_setopt($curl, CURLOPT_URL, $url);
-  curl_setopt($curl, CURLOPT_HTTPHEADER, array(
-    'Content-Type: application/json',
-  ));
-
-  curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-  curl_setopt($curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
-
-  //Execute
-  $result = curl_exec($curl);
-  if(!$result){die("Connection Failure");}
-  curl_close($curl);
-  return $result;
-
-
-}
-
-
 ?>

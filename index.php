d3c2c4b11afab15f1a2fd45df9812d395623db28
Andy <andrew@mcgee.technology>
2021-02-03 22:02:12 +1100

    Column widths

--- index.php
+++ index.php
@@ -17,7 +17,7 @@
 <!-- Main Body -->
 	<!-- 2 column grid -->
 	<div class="ui two column grid">
-		<div class="ui three wide sidemenu column"><!-- Start of page column 1 - left sidebar -->
+		<div class="ui two wide sidemenu column"><!-- Start of page column 1 - left sidebar -->
 			<div class="ui container"> <!-- Container to constrain width of Vertical Menu -->
 
 			<!-- Include the side menu code -->
@@ -29,7 +29,7 @@
 			</div> <!-- Close container -->
 		</div> <!-- End of Column 1 -->
 
-    <div class="ui thirteen wide column"><!-- START of main column 2 -->
+    <div class="ui fourteen wide column"><!-- START of main column 2 -->
 		  <div class="ui inverted manatee segment">
 			  <div class="ui inverted space segment">
 			    <h1 class="ui smoke header">Recent Albums</h1>

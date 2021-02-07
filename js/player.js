c011e7e50f89e4dec38003457b692c2aac793197
Andy <andrew@mcgee.technology>
2021-02-07 23:33:05 +1100

    Fixing muted - global const

--- js/player.js
+++ js/player.js
@@ -4,7 +4,7 @@
  */
 
 const setVol = 1.0;
-const muted = false;
+const this.muted = false;
 
 //Define some player functions
 
@@ -50,12 +50,12 @@ function mute() {
     // if muted then unmute the sound & show volume button
     trk01.mute(false);
     document.getElementById("volBtn").className = "bordered volume up icon";
-    muted = false;
+    this.muted = false;
   } else {
     // if unmuted then mute the sound & show mute button
     trk01.mute(true);
     document.getElementById("volBtn").className = "bordered volume mute icon";
-    muted = true;
+    this.muted = true;
   }
 }
 

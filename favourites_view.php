5184c6231d18fe07b4ae6529e7ec9ed9acaaaff2
Andy <andrew@mcgee.technology>
2021-02-28 18:39:14 +1100

    Added favourites_view.

---
+++ favourites_view.php
@@ -0,0 +1,45 @@
+<?php
+	include 'includes/callAPI.php';
+
+	$get_data = handshakeAPI();
+	$hshake = json_decode($get_data, true);
+
+	$auth=$hshake[auth];
+
+	$get_data = statsAPI($auth, 'flagged');
+	$results = json_decode($get_data, true);
+
+	include 'includes/header_iframe.php';
+?>
+<script>parent.activeMenu(8);</script> <!-- Call js function in parent to highlight the correct active menu item -->
+
+<body style="overflow:hidden">
+			  <div class="ui inverted space segment">
+			    <h1 class="ui smoke header">Favourite Albums</h1>
+
+					<?php
+					$cnt = 0; //Reset our counter to build grid of 24 entries
+					echo "<div class='ui six column grid container'>";
+					//Loop 4 rows
+					for ($i = 1; $i <=4; $i++){
+						echo "<div class='ui row'>";
+						//Loop 6 columns
+						for ($j = 1; $j <=6; $j++){
+							echo "<div class='ui column'>";
+							echo '<a href="album_view.php?uid=' . $results[album][$cnt][id] . '">';
+							echo "<img class='ui small image' src='" . $results[album][$cnt][art] . "' ></a>";
+							echo '<br><center><a href="album_view.php?uid=' . $results[album][$cnt][id] . '">';
+							echo $results[album][$cnt][name] . "</a>";
+							echo '<br><a href="artist_albums.php?uid=' . $results[album][$cnt][artist][id] . '">';
+							echo $results[album][$cnt][artist][name] . "</a>";
+							echo '<br>'. $results[album][$cnt][year];
+							echo "</center></div>";
+							$cnt++; //Increment our counter
+						}
+						echo "</div>";
+					}
+					echo "</div>";
+					?>
+				</div>
+</body>
+</html>

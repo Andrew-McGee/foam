ec520527f0b1fca92bf2420e0150de956ca3a917
Andy <andrew@mcgee.technology>
2021-03-20 21:38:01 +1100

    Added hidden elipsis menu to track listings in artist track view.

--- artist_tracks.php
+++ artist_tracks.php
@@ -16,8 +16,23 @@
 
 	include 'includes/header_iframe.php';
 ?>
-<script>parent.activeMenu(0);</script> <!-- Call js function in parent to highlight the correct active menu item -->
+<script>
+parent.activeMenu(0); // Call js function in parent to highlight the correct active menu item -->
+parent.list = []; // Clean out the old list before we build a new one
 
+// Function to hide star and elipse on track listing row
+function hideicon(num) {
+		if (document.getElementById("hiddenstar"+num).className !== "blue star icon") {
+    	document.getElementById("hiddenstar"+num).setAttribute("style", "opacity:0 !important");
+		}
+    document.getElementById("hiddenelipse"+num).setAttribute("style", "opacity:0 !important");
+}
+// Function to reveal star and elipse on track listing row
+function revealicon(num) {
+    document.getElementById("hiddenstar"+num).setAttribute("style", "opacity:1 !important");
+    document.getElementById("hiddenelipse"+num).setAttribute("style", "opacity:1 !important");
+}
+</script>
 <body style="overflow:hidden">
 			  <div class="ui inverted space segment">
 					<?php
@@ -36,7 +51,11 @@
 								echo '<div class="ui huge smoke header">' . $artist_results['name'] . '</div>';
 								echo '<button class="ui tiny grey button" id="playb"><i class="play icon"></i>PLAY</button>';
 								echo '&nbsp;<button class="ui tiny grey button" id="shufb"><i class="random icon"></i>SHUFFLE</button>';
-								echo '&nbsp;<i class="ellipsis vertical icon"></i>';
+								echo '&nbsp;<div class="ui inline dropdown"><i class="ellipsis vertical icon"></i>';
+								echo '	<div class="menu" id="albumMenu">';
+								echo '	<div class="item">Play next</div>';
+								echo '	<div class="item">Add to queue</div>';
+								echo '</div></div>';
 								// Make a listener for clicking on the play button
 								echo "<script>playb.addEventListener('click', function() {";
 								echo "  parent.newQueue('0');";
@@ -50,21 +69,42 @@
 								//Let's make the table for the song list
 								echo '<table class="ui selectable inverted black table">';
 								echo '<thead><tr>';
-								echo '<th>Title</th><th>Album</th><th>Time</th><th>DL</th>';
+								echo '<th>Title</th><th>Album</th><th></th><th>Time</th><th>DL</th>';
 								echo '</tr></thead>';
 								echo '<tbody>';
 								$cnt = $artist_results['songcount']; //Set counter to number of songs on album
 
 								//Loop through the songs to display each on a table row
 								for ($i = 0; $i < $cnt; $i++){
-									echo '<tr>';
-									echo '<td id="trk' . ($i + 1) . '"><strong>' . $song_results['song'][$i]['title'] . '</strong></td>';
-									echo '<td><a href="album_view.php?uid=' . $song_results['song'][$i]['album']['id'] . '">';
-									echo $song_results['song'][$i]['album']['name'] . '</a></td>';
-									$result = sec2mins($song_results['song'][$i]['time']);
-									echo '<td>' . $result['minutes'] . ':' . sprintf("%02d", $result['seconds']) . '</td>';
-									echo '<td><a href="' . $song_results['song'][$i]['url'] . '"><i class="download icon"></i></a></td>';
-									echo '</tr>';
+									echo '<tr id="row' . $i . '">'; // Start of the track listing row
+										echo '<td id="trk' . ($i + 1) . '"><strong>' . $song_results['song'][$i]['title'] . '</strong></td>';
+										echo '<td><a href="album_view.php?uid=' . $song_results['song'][$i]['album']['id'] . '">';
+										echo $song_results['song'][$i]['album']['name'] . '</a></td>';
+
+											// hidden star and elipse reveal on mouseover row (see listeners below)
+											// some code here to test if song is flagged or not (favourite = blue star)
+											$fav = $song_results['song'][$i]['flag'];
+											if ($fav == true) {
+												$favi = "blue star icon";
+											} else {
+												$favi = "hidden star outline icon";
+											}
+
+											echo '<td><i class="' . $favi . '" id="hiddenstar' . $i . '"></i>&nbsp';
+											echo '<div class="ui inline dropdown"><i class="hidden ellipsis vertical icon" id="hiddenelipse' . $i . '"></i>';
+											echo '	<div class="menu" id="albumMenu">';
+											echo '		<div class="item" id="playNext' . $i . '">Play next</div>';
+											echo '		<div class="item" id="addT2Q' . $i . '">Add to queue</div>';
+											echo '		<div class="item">Go to album</div>';
+											echo '		<div class="item">Go to artist</div>';
+											echo '	</div>';
+											echo '</div></td>';
+
+										// Calculate song length from seconds
+										$result = sec2mins($song_results['song'][$i]['time']);
+										echo '<td>' . $result['minutes'] . ':' . sprintf("%02d", $result['seconds']) . '</td>';
+										echo '<td><a href="' . $song_results['song'][$i]['url'] . '"><i class="download icon"></i></a></td>';
+									echo '</tr>'; // End of the row but theres some other stuff still to do in this loop
 
 									// Let's add this entry to our js 'list' array in case it becomes a new playlist or queue
 									echo '<script>';
@@ -80,6 +120,27 @@
 									echo "<script>trk" . ($i + 1) . ".addEventListener('click', function() {";
 									echo "  parent.newQueue('" . $i . "');";
 									echo '});</script>';
+
+									// Make a listener for hovering over a row to make star and elipse visible
+									echo "<script>row" . $i . ".addEventListener('mouseover', function() {";
+									echo "	revealicon('" . $i . "');";
+									echo "});</script>";
+
+									// Make a listener for moving off a row to make star and elipse invisible
+									echo "<script>row" . $i . ".addEventListener('mouseout',  function() {";
+									echo "	hideicon('" . $i . "');";
+									echo "});</script>";
+
+									// Make a listener for add track to queue (addT2Q) menu item
+									echo "<script>addT2Q" . $i . ".addEventListener('click',  function() {";
+									echo "	parent.addT2Q('" . $i . "');";
+									echo "});</script>";
+
+									// Make a listener for playNext menu item
+									echo "<script>playNext" . $i . ".addEventListener('click',  function() {";
+									echo "	parent.playNext('" . $i . "');";
+									echo "});</script>";
+
 								}//End of row loop
 
 								echo '</tbody></table>';
@@ -88,5 +149,11 @@
 						echo '</div>'; //End of content grid.
 					?>
 			  </div>
+<!-- JS to initialise dropdowns-->
+<script>
+$('.ui.dropdown')
+  .dropdown()
+;
+</script>
 </body>
 </html>

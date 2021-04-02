431619706dc536a665b8856b2e2eb531d4eb5538
Andy <andrew@mcgee.technology>
2021-04-02 16:00:08 +1100

    Fix quotes in song title bug. Source readability with /r/n.

--- album_view.php
+++ album_view.php
@@ -36,10 +36,10 @@ function revealicon(num) {
 <body style="overflow:hidden">
 			  <div class="ui inverted space segment">
 					<?php
-						echo '<div class="ui two column grid">'; //Two columns for album view - art on left, tracks on right.
+						echo '<div class="ui two column grid">' . "\r\n"; //Two columns for album view - art on left, tracks on right.
 
 							// Left column for album art and stats
-							echo '<div class="ui four wide column">';
+							echo '<div class="ui four wide column">' . "\r\n";
 								echo "<img class='ui massive image' src='" . $albm_results['art'] . "' >";
 								echo '<br><a href="artist_albums.php?uid=' . $albm_results['artist']['id'] . '">';
 								echo $albm_results['artist']['name'] . '</a>';
@@ -57,48 +57,48 @@ function revealicon(num) {
 								}
 								//echo '<br>time element = ' . $albm_results['album'][0]['time'];
 								//var_dump($albm_results);
-							echo '</div>'; // End of 1st column
+							echo '</div>' . "\r\n"; // End of 1st column
 
 							// Right column for album songs in table
-							echo '<div class="ui twelve wide column">';
-								echo '<div class="ui huge smoke header">' . $albm_results['name'] . '</div>';
+							echo '<div class="ui twelve wide column">' . "\r\n";
+								echo '<div class="ui huge smoke header">' . $albm_results['name'] . '</div>' . "\r\n";
 								echo '<button class="ui tiny button" id="playb"><i class="play icon"></i>PLAY</button>';
 								echo '&nbsp;<button class="ui tiny button" id="shufb"><i class="random icon"></i>SHUFFLE</button>';
-								echo '&nbsp;<div class="ui inline dropdown"><i class="ellipsis vertical icon"></i>';
-								echo '	<div class="menu" id="albumMenu">';
-								echo '	<div class="item" id="addAll2Q">Add to queue</div>';
-								echo '	<div class="item">Play next</div>';
-								echo '</div></div>';
+								echo '&nbsp;<div class="ui inline dropdown"><i class="ellipsis vertical icon"></i>' . "\r\n";
+								echo '	<div class="menu" id="albumMenu">' . "\r\n";
+								echo '	<div class="item" id="addAll2Q">Add to queue</div>' . "\r\n";
+								echo '	<div class="item">Play next</div>' . "\r\n";
+								echo '</div></div>' . "\r\n";
 								// Make a listener for clicking on the play button
-								echo "<script>playb.addEventListener('click', function() {";
+								echo "\r\n<script>playb.addEventListener('click', function() {";
 								echo "  parent.newQueue('0');";
-								echo '});</script>';
+								echo '});</script>' . "\r\n";
 
 								// Make a listener for clicking on the shuffle button
 								echo "<script>shufb.addEventListener('click', function() {";
 								echo "  parent.shuffle('0');";
-								echo '});</script>';
+								echo '});</script>' . "\r\n";
 
 								// Make a listener for Add to Queue menu item
 								echo "<script>addAll2Q.addEventListener('click', function() {";
 								echo "	parent.addAll2Q();";
-								echo '});</script>';
+								echo '});</script>' . "\r\n";
 
 								//Let's make the table for the song list
-								echo '<table class="ui selectable inverted black table">';
+								echo "\r\n" . '<table class="ui selectable inverted black table">' . "\r\n";
 								echo '<thead><tr>';
 								echo '<th>#</th><th>Title</th><th>Artist</th><th></th><th>Time</th><th>DL</th>';
-								echo '</tr></thead>';
-								echo '<tbody>';
+								echo '</tr></thead>' . "\r\n";
+								echo '<tbody>' . "\r\n";
 								$cnt = $albm_results['songcount']; //Set counter to total number of songs on album
 
 								//Loop through the songs to display each on a table row
 								for ($i = 0; $i < $cnt; $i++){
-									echo '<tr id="row' . $i . '">'; // Start of the track listing row
-										echo '<td id="tno' . ($i + 1) . '">' . $song_results['song'][$i]['track'] . '</td>';
-										echo '<td id="trk' . ($i + 1) . '"><strong>' . $song_results['song'][$i]['title'] . '</strong></td>';
+									echo "\r\n" . '<tr id="row' . $i . '">' . "\r\n"; // Start of the track listing row
+										echo '<td id="tno' . ($i + 1) . '">' . $song_results['song'][$i]['track'] . '</td>' . "\r\n";
+										echo '<td id="trk' . ($i + 1) . '"><strong>' . $song_results['song'][$i]['title'] . '</strong></td>' . "\r\n";
 										echo '<td><a href="artist_albums.php?uid=' . $song_results['song'][$i]['artist']['id'] . '">';
-										echo $song_results['song'][$i]['artist']['name'] . '</a></td>';
+										echo $song_results['song'][$i]['artist']['name'] . '</a></td>' . "\r\n";
 
 										// hidden star and elipse reveal on mouseover row (see listeners below)
 										// some code here to test if song is flagged or not (favourite = blue star)
@@ -109,76 +109,77 @@ function revealicon(num) {
 											$favi = "hidden star outline icon";
 										}
 
-										echo '<td><i class="' . $favi . '" id="hiddenstar' . $i . '"></i>&nbsp';
-										echo '<div class="ui inline dropdown"><i class="hidden ellipsis vertical icon" id="hiddenelipse' . $i . '"></i>';
-										echo '	<div class="menu" id="albumMenu">';
-										echo '		<div class="item" id="addT2Q' . $i . '">Add to queue</div>';
-										echo '		<div class="item" id="playNext' . $i . '">Play next</div>';
-										echo '		<div class="item" id="playOnly' . $i . '">Play only</div>';
-										echo '		<div class="item"><a href="album_view.php?uid=' . $song_results['song'][$i]['album']['id'] . '">Go to album</a></div>';
-										echo '		<div class="item"><a href="artist_albums.php?uid=' . $song_results['song'][$i]['artist']['id'] . '">Go to artist</a></div>';
-										echo '	</div>';
-										echo '</div></td>';
+										echo '<td><i class="' . $favi . '" id="hiddenstar' . $i . '"></i>&nbsp;' . "\r\n";
+										echo '<div class="ui inline dropdown"><i class="hidden ellipsis vertical icon" id="hiddenelipse' . $i . '"></i>' . "\r\n";
+										echo '	<div class="menu" id="albumMenu">' . "\r\n";
+										echo '		<div class="item" id="addT2Q' . $i . '">Add to queue</div>' . "\r\n";
+										echo '		<div class="item" id="playNext' . $i . '">Play next</div>' . "\r\n";
+										echo '		<div class="item" id="playOnly' . $i . '">Play only</div>' . "\r\n";
+										echo '		<div class="item"><a href="album_view.php?uid=' . $song_results['song'][$i]['album']['id'] . '">Go to album</a></div>' . "\r\n";
+										echo '		<div class="item"><a href="artist_albums.php?uid=' . $song_results['song'][$i]['artist']['id'] . '">Go to artist</a></div>' . "\r\n";
+										echo '	</div>' . "\r\n";
+										echo '</div></td>' . "\r\n";
 
 										// Calculate song length from seconds
 										$result = sec2mins($song_results['song'][$i]['time']);
-										echo '<td>' . $result['minutes'] . ':' . sprintf("%02d", $result['seconds']) . '</td>';
+										echo '<td>' . $result['minutes'] . ':' . sprintf("%02d", $result['seconds']) . '</td>' . "\r\n";
 
-										echo '<td><a href="' . $song_results['song'][$i]['url'] . '"><i class="download icon"></i></a></td>';
-									echo '</tr>'; // End of the row but theres some other stuff still to do in this loop
+										echo '<td><a href="' . $song_results['song'][$i]['url'] . '"><i class="download icon"></i></a></td>' . "\r\n";
+									echo '</tr>' . "\r\n"; // End of the row but theres some other stuff still to do in this loop
 
 									// Let's add this entry to our js 'list' array in case it becomes a new playlist or queue
-									echo '<script>';
-									echo 'parent.list[' . $i . '] = [];';
-									echo 'parent.list[' . $i . '][0] = "' . $song_results['song'][$i]['title'] . '";';
-									echo 'parent.list[' . $i . '][1] = "' . $song_results['song'][$i]['artist']['name'] . '";';
-									echo 'parent.list[' . $i . '][2] = "' . $result['minutes'] . ':' . sprintf("%02d", $result['seconds']) . '";';
-									echo 'parent.list[' . $i . '][3] = "' . $albm_results['art'] . '";';
-									echo 'parent.list[' . $i . '][4] = "' . $song_results['song'][$i]['url'] . '";';
-									echo '</script>';
+									$safeTitle = addslashes($song_results['song'][$i]['title']);  // escapes any quotes in the title
+									echo "\r\n" . '<script>' . "\r\n";
+									echo ' parent.list[' . $i . '] = [];' . "\r\n";
+									echo ' parent.list[' . $i . '][0] = "' . $safeTitle . '";' . "\r\n";
+									echo ' parent.list[' . $i . '][1] = "' . $song_results['song'][$i]['artist']['name'] . '";' . "\r\n";
+									echo ' parent.list[' . $i . '][2] = "' . $result['minutes'] . ':' . sprintf("%02d", $result['seconds']) . '";' . "\r\n";
+									echo ' parent.list[' . $i . '][3] = "' . $albm_results['art'] . '";' . "\r\n";
+									echo ' parent.list[' . $i . '][4] = "' . $song_results['song'][$i]['url'] . '";' . "\r\n";
+									echo '</script>' . "\r\n";
 
 									// ** TEMP NOTE ** Remove these duplicate functions and create single named functions for better performance
 									// Make a listener for clicking on this track title
 									echo "<script>trk" . ($i + 1) . ".addEventListener('click', function() {";
 									echo "  parent.newQueue('" . $i . "');";
-									echo '});</script>';
+									echo '});</script>' . "\r\n";
 
 									// Make a listener for clicking on this track number
 									echo "<script>tno" . ($i + 1) . ".addEventListener('click', function() {";
 									echo "  parent.newQueue('" . $i . "');";
-									echo '});</script>';
+									echo '});</script>' . "\r\n";
 
 									// Make a listener for hovering over a row to make star and elipse visible
 									echo "<script>row" . $i . ".addEventListener('mouseover', function() {";
 									echo "	revealicon('" . $i . "');";
-									echo "});</script>";
+									echo '});</script>' . "\r\n";
 
 									// Make a listener for moving off a row to make star and elipse invisible
 									echo "<script>row" . $i . ".addEventListener('mouseout',  function() {";
 									echo "	hideicon('" . $i . "');";
-									echo "});</script>";
+									echo '});</script>' . "\r\n";
 
 									// Make a listener for add track to queue (addT2Q) menu item
 									echo "<script>addT2Q" . $i . ".addEventListener('click',  function() {";
 									echo "	parent.addT2Q('" . $i . "');";
-									echo "});</script>";
+									echo '});</script>' . "\r\n";
 
 									// Make a listener for playNext menu item
 									echo "<script>playNext" . $i . ".addEventListener('click',  function() {";
 									echo "	parent.playNext('" . $i . "');";
-									echo "});</script>";
+									echo '});</script>' . "\r\n";
 
 									// Make a listener for playOnly menu item
 									echo "<script>playOnly" . $i . ".addEventListener('click',  function() {";
 									echo "	parent.newSingle('" . $i . "');";
-									echo "});</script>";
+									echo '});</script>' . "\r\n";
 
 								} //End of row loop
 
-								echo '</tbody></table>';
-							echo '</div>'; // End of 2nd column
+								echo '</tbody></table>' . "\r\n";
+							echo '</div>' . "\r\n"; // End of 2nd column
 
-						echo '</div>'; //End of content grid.
+						echo '</div>' . "\r\n"; //End of content grid.
 					?>
 			  </div>
 <!-- JS to initialise dropdowns-->

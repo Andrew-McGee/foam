623cb0ddc2b38a030fe39ef7eb6bc4b65d4faad0
Andy <andrew@mcgee.technology>
2021-05-03 21:52:51 +1000

    config updates.

--- config/foam.conf.php
+++ config/foam.conf.php
@@ -6,10 +6,24 @@ $ampurl = 'https://localhost:8080';
 $ampusr = 'username';
 $amppas = 'passwd';
 
-//Themes - set some basic colours from Fomantic UI
+//Themes - some basic pre-set colour schemes
 
-$txtcolr1 = 'black';
-$elmcolr1 = 'blue';
-$elmcolr2 = 'green';
+// #01 Original
+$theme = array();
+$theme[1][colrfgd1] = '#2a2c41'; // Space Cadet
+$theme[1][colrbgd1] = '#556177'; // Manatee
+$theme[1][colrfnt1] = '#A5A5A5'; // Main grey text
+$theme[1][colrfnt2] = '#F5F5F5'; // WhiteSmoke - highlight text and headings
+$theme[1][colrfnt3] = '#556177'; // Manatee - Now playing text
+$theme[1][colrhilt] = '#0E6EB8'; // Blue highlight
+
+// #02 New
+$theme = array();
+$theme[2][colrfgd1] = '#2a2c41'; // Space Cadet
+$theme[2][colrbgd1] = '#556177'; // Manatee
+$theme[2][colrfnt1] = '#A5A5A5'; // Main grey text
+$theme[2][colrfnt2] = '#F5F5F5'; // WhiteSmoke - highlight text and headings
+$theme[2][colrfnt3] = '#556177'; // Manatee - Now playing text
+$theme[2][colrhilt] = '#0E6EB8'; // Blue highlight
 
 ?>

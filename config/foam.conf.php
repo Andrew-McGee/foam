714e8aafaba8d2912c067655aed464c63ce2f99d
Andy <andrew@mcgee.technology>
2021-05-09 23:27:28 +1000

    Fixed array names in quotes.

--- config/foam.conf.php
+++ config/foam.conf.php
@@ -13,19 +13,19 @@ $sessionTime = 365 * 24 * 3600; // Seconds in 1 year
 $theme = array();
 
 // #01 Original
-$theme[1][colrfgd1] = '#2a2c41'; // Space Cadet
-$theme[1][colrbgd1] = '#556177'; // Manatee
-$theme[1][colrfnt1] = '#A5A5A5'; // Main grey text
-$theme[1][colrfnt2] = '#F5F5F5'; // WhiteSmoke - highlight text and headings
-$theme[1][colrfnt3] = '#556177'; // Manatee - Now playing text
-$theme[1][colrhilt] = '#0E6EB8'; // Blue highlight
+$theme[1]['colrfgd1'] = '#2a2c41'; // Space Cadet
+$theme[1]['colrbgd1'] = '#556177'; // Manatee
+$theme[1]['colrfnt1'] = '#A5A5A5'; // Main grey text
+$theme[1]['colrfnt2'] = '#F5F5F5'; // WhiteSmoke - highlight text and headings
+$theme[1]['colrfnt3'] = '#556177'; // Manatee - Now playing text
+$theme[1]['colrhilt'] = '#0E6EB8'; // Blue highlight
 
 // #02 New
-$theme[2][colrfgd1] = '#2a2c41'; // Space Cadet
-$theme[2][colrbgd1] = '#556177'; // Manatee
-$theme[2][colrfnt1] = '#A5A5A5'; // Main grey text
-$theme[2][colrfnt2] = '#F5F5F5'; // WhiteSmoke - highlight text and headings
-$theme[2][colrfnt3] = '#556177'; // Manatee - Now playing text
-$theme[2][colrhilt] = '#0E6EB8'; // Blue highlight
+$theme[2]['colrfgd1'] = '#2a2c41'; // Space Cadet
+$theme[2]['colrbgd1'] = '#556177'; // Manatee
+$theme[2]['colrfnt1'] = '#A5A5A5'; // Main grey text
+$theme[2]['colrfnt2'] = '#F5F5F5'; // WhiteSmoke - highlight text and headings
+$theme[2]['colrfnt3'] = '#556177'; // Manatee - Now playing text
+$theme[2]['colrhilt'] = '#0E6EB8'; // Blue highlight
 
 ?>

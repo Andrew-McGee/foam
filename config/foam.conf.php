7dbc58b22991520924bb5865e57e696f88d54f7f
Andy <andrew@mcgee.technology>
2021-05-09 17:18:33 +1000

    Testing theme array in conf file.

--- config/foam.conf.php
+++ config/foam.conf.php
@@ -10,9 +10,9 @@ $sessionTime = 365 * 24 * 3600; // Seconds in 1 year
 // $sessionTime = 3600;            // Seconds in 1 hour
 
 //Themes - some basic pre-set colour schemes
+$theme = array();
 
 // #01 Original
-$theme = array();
 $theme[1][colrfgd1] = '#2a2c41'; // Space Cadet
 $theme[1][colrbgd1] = '#556177'; // Manatee
 $theme[1][colrfnt1] = '#A5A5A5'; // Main grey text
@@ -21,7 +21,6 @@ $theme[1][colrfnt3] = '#556177'; // Manatee - Now playing text
 $theme[1][colrhilt] = '#0E6EB8'; // Blue highlight
 
 // #02 New
-$theme = array();
 $theme[2][colrfgd1] = '#2a2c41'; // Space Cadet
 $theme[2][colrbgd1] = '#556177'; // Manatee
 $theme[2][colrfnt1] = '#A5A5A5'; // Main grey text

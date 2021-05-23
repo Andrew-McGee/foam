39ce8bd5aaf115c2d526e9fce2e66bc8a54843f8
Andy <andrew@mcgee.technology>
2021-05-24 00:18:01 +1000

    Theme changes and css variable tweaks.

--- config/foam.conf.php
+++ config/foam.conf.php
@@ -21,13 +21,13 @@ $sessionTime = 365 * 24 * 3600; // Seconds in 1 year
   $theme[1]['colrfnt3'] = '#556177'; // Manatee - Now playing text
   $theme[1]['colrhilt'] = '#0E6EB8'; // Blue highlight
 
-  // #02 Flip - Uncomment to use
-  $theme[2]['name'] = 'Flip';     // Name
-  $theme[2]['colrfgd1'] = '#556177'; // Manatee
-  $theme[2]['colrbgd1'] = '#2a2c41'; // Space Cadet
+  // #02 GoA - Uncomment to use
+  $theme[2]['name'] = 'GoA';     // Name
+  $theme[2]['colrfgd1'] = '#27293D'; // Manatee
+  $theme[2]['colrbgd1'] = '#1E1E2F'; // Space Cadet
   $theme[2]['colrfnt1'] = '#A5A5A5'; // Main grey text
   $theme[2]['colrfnt2'] = '#F5F5F5'; // WhiteSmoke - highlight text and headings
-  $theme[2]['colrfnt3'] = '#556177'; // Manatee - Now playing text
+  $theme[2]['colrfnt3'] = '#707070'; // Manatee - Now playing text
   $theme[2]['colrhilt'] = '#0E6EB8'; // Blue highlight
 
   // #03 Forest - Uncomment to use

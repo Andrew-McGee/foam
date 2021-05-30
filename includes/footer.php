7fd500e0cfc4d93dccf9864d4c87a2be8a0ed558
Andy <andrew@mcgee.technology>
2021-05-30 17:46:51 +1000

    Get rid of footer - include in index.php.

--- includes/footer.php
+++ includes/footer.php
@@ -1,11 +1,6 @@
 <!-- Footer for foam - Copyright 2021 Andrew McGee -->
 
 <!-- Start the footer - note this closes out the body and html tags -->
-  <div class="ui inverted spacecadet vertical footer segment">
-    <div class="ui center aligned container">
-			<h1 class="ui inverted yellow header">foam</h1>
-    </div>
-  </div>
 
 <!-- JS to initialise dropdowns-->
 <script>

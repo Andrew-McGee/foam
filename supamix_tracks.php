b591c9408e141529dccad5c8d479acb0704df767
Andy <andrew@mcgee.technology>
2022-09-11 08:29:43 +1000

    Updates to fix new playlists call.

--- supamix_tracks.php
+++ supamix_tracks.php
@@ -24,7 +24,7 @@
 	$total_supalist = count($supalist);
 
 	// Get playlist info so we can have them listed in our per track menu
-	$get_data = playlistsAPI($auth);
+	$get_data = playlistsAPI($auth, '', 0);
 	$playlist_results = json_decode($get_data, true);
 
 	include 'includes/header_iframe.php';
